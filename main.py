64bb1b2809009f12c650b354c8e677d44d3e18fc
Adam Bowman <adam.bowman@atos.net>
2023-08-08 14:40:44 +0100

    Better log messages

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ try:
   support_match_patterns = support_regex_match_wiki_page.content_md.strip().split('\n')
   support_exclude_patterns = support_regex_exclude_wiki_page.content_md.strip().split('\n')
   
-  logger.info("Init complete")
+  logger.info(f"Init complete: logged in as {reddit_username} monitoring {subreddit_name}")
 except Exception as e:
   logger.error(f"Encountered an exception during startup: {e}")
   quit()
@@ -85,9 +85,9 @@ def handle_current_flair(user, new_points):
     # check if they have an existing flair and find the flair text
     for flair in subreddit.flair(user):
         logger.info("Found user's existing flair")
-        if flair["flair_text"] is not None:
+        if flair["flair_text"]:
             user_flair_text = flair["flair_text"]
-            logger.debug(f"Existing flair text is {user_flair_text}")
+            logger.debug(f"Existing flair text is {user_flair_text.replace('★ ', '')}")
             break
     # if their flair text is nothing
     if not user_flair_text:
@@ -96,7 +96,7 @@ def handle_current_flair(user, new_points):
     # if a star already exists in the flair, increment the thanks count
     elif str("★") in user_flair_text:
         cur_points = int(user_flair_text.split(" ")[-1])
-        logger.info("Thanks flair set, incrementing {str(cur_points)} to {str(new_points)}")
+        logger.info(f"Thanks flair set, incrementing {str(cur_points)} to {str(new_points)}")
         user_flair_text = user_flair_text.replace(str(cur_points), str(new_points))
     else:
         logger.debug("Custom flair detected")
@@ -153,11 +153,12 @@ def set_wiki_leaderboard(df, user_exists_in_leaderboard, user, points):
     user = f"u/{user}"
     today = date.today()
     if user_exists_in_leaderboard:
+        logger.debug("User located in table, updating existing row")
         # update the level and date cells on the row matching the username
         df.loc[df["Username"] == user, "Level"] = f"★ {points}"
         df.loc[df["Username"] == user, "Last Star Date"] = today.strftime("%Y-%m-%d")
     else:
-        logger.debug("User not located in table")
+        logger.debug("User not located in table, adding")
         new_row = {
             "Username": user,
             "Level": f"★ {points}",
@@ -183,12 +184,12 @@ def thank_user(user):
   df = get_wiki_leaderboard()
   user_exists_in_leaderboard = df[df["Username"] == f"u/{user}"]
   if not user_exists_in_leaderboard.empty:
-      logger.debug("User: {user} exists in leaderboard, so incrementing existing points")
+      logger.debug(f"User: {user} exists in leaderboard, so incrementing existing points")
       level = user_exists_in_leaderboard["Level"].iloc[0]
       last_star_date = user_exists_in_leaderboard["Last Star Date"].iloc[0]
       points = int(level.split(" ")[-1]) + 1
   else:
-      logger.debug("User: {user} doesn't exist in leaderboard, so points = 1")
+      logger.debug(f"User: {user} doesn't exist in leaderboard, so points = 1")
       points = 1
   user_flair_text = handle_current_flair(user, points)
   set_wiki_leaderboard(df, not user_exists_in_leaderboard.empty, user, points)
